519ba47fe701bb601eacb3ae307c97e3f55bf953
liuwei <499271211@qq.com>
2022-07-28 20:52:15 +0800

    liuwei的测试02

--- src/liuwei.java
+++ src/liuwei.java
@@ -2,5 +2,6 @@ public class liuwei {
     public static void main(String[] args) {
         System.out.println("Hello YOU");
         System.out.println("yyds");
+        System.out.println("66666");
     }
 }
