eb79fa8ce124231eb9ccb97f4210e7ff146be8b3
liuwei <499271211@qq.com>
2022-07-28 20:55:28 +0800

    liuwei的更新

--- src/liuwei.java
+++ src/liuwei.java
@@ -2,6 +2,6 @@ public class liuwei {
     public static void main(String[] args) {
         System.out.println("Hello YOU");
         System.out.println("yyds");
-        System.out.println("66666");
+        System.out.println("6666666");
     }
 }
