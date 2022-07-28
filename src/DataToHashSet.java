aa7d31db2c5836791c1589dddf1ad996c8d280cc
mlb <172956419@QQ.COM>
2022-07-28 19:55:24 +0800

    123456789

--- src/DataToHashSet.java
+++ src/DataToHashSet.java
@@ -7,7 +7,5 @@ public class DataToHashSet {
         numSet.add("12,45,23,86,100,78,546,1,45,99,136,23");
         System.out.println(numSet);
         System.out.println("mlb");
-        System.out.println("mlb");
-        System.out.println("mlb");
     }
 }
